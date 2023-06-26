ba8bfaf627f7aee9ca9bdce89c551a00edeff239
Iulia Ciobanu <iuliaciobanu2804@gmail.com>
2023-06-26 19:42:15 +0300

    Strings ex2

---
+++ Strings ex2/Strings ex2/Program.cs
@@ -0,0 +1,19 @@
+﻿//Write a method that to remove the characters which have odd index values of a given string
+public static class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Your string is:");
+        string str=Console.ReadLine();
+        string Str = null;
+        for(int i=0; i<str.Length; i++)
+        {
+            if (i % 2 == 0)
+            {
+                Str = Str + str[i];
+            }
+        }
+        Console.WriteLine("The new string is:" +Str);
+    }
+
+}
