a213f7a59b7f04b760472a332942dcfb4aba02e5
Iulia Ciobanu <iuliaciobanu2804@gmail.com>
2023-06-26 19:04:46 +0300

    Strings ex1

---
+++ Strings ex1/StringsEx1/Program.cs
@@ -0,0 +1,15 @@
+﻿//Write a method that to remove the nth index character from a nonempty string.
+public static class Program
+{
+    public static void Main()
+    {
+        Console.Write("Input your string:");
+        string str=Console.ReadLine(); ;
+
+        Console.Write("N'th index is:"); 
+        int n=int.Parse(Console.ReadLine()) ;
+
+        string Str=str.Remove(n-1,1) ;
+        Console.WriteLine("The new string is:"+ Str) ;
+    }
+}
