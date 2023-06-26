b6d4ff0a11c2317b14d5763e80a288272c61faac
Iulia Ciobanu <iuliaciobanu2804@gmail.com>
2023-06-26 18:05:40 +0300

    ex1

---
+++ ex1/ex1/Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+//1.Write a program that reads from the console 3 numbers and prints their sum
+public static class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Entre three numbers:");
+
+        Console.WriteLine("number 1=");
+        int number1 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("number2=");
+        int number2 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("number3=");
+        int number3 = int.Parse(Console.ReadLine());
+
+        int sum = number1 + number2 + number3;
+        Console.WriteLine("The sum of the three numbers is:" + sum);
+    }
+}
